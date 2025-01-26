dea414aa44be6c32e898445dde6e3afc037a884d
Ezerskaya <ezersk@gmail.com>
2025-01-26 16:54:16 +0300

    fix: delete hook & change logic hook & rename variable

--- src/components/restaurant/Restaurant.jsx
+++ src/components/restaurant/Restaurant.jsx
@@ -12,15 +12,11 @@ const cardStyle = {
 export const Restaurant = ({ restaurant }) => {
   const { id, name } = restaurant
 
-  const checkEmptyReviews = restaurant.reviews.length !== 0;
-
   return (
     <div key={id} style={cardStyle}>
       <h2>{name}</h2>
       <RestaurantMenu restaurant={restaurant}/>
-      {checkEmptyReviews && (
-        <RestaurantReviews restaurant={restaurant}/>
-      )}
+      <RestaurantReviews restaurant={restaurant}/>
     </div>
   )
 }
